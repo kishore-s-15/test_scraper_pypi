86405bb688e9959d9f3fd39bac9987c1dba14623
kishore-s-15 <skishore2602.dev@gmail.com>
2022-05-28 17:19:35 +0530

    Modified TestScraperPypi

--- test_scraper_pypi/test_scraper_pypi.py
+++ test_scraper_pypi/test_scraper_pypi.py
@@ -1,8 +1,11 @@
 """Main module."""
-
+from typing import Optional
 
 class TestScraperPypi:
     """Main class."""
+
+    def __init__(self, name: Optional[str] = None):
+        self.name = name
 
     def main(self):
         """Main function."""
@@ -10,5 +13,9 @@ class TestScraperPypi:
 
     def return_greet_message(self):
         """Return a greeting message."""
-        message = "Hello World!"
+
+        message = "Hello "
+        message += self.name if self.name is not None else "World"
+        message += "!"
+
         return message
