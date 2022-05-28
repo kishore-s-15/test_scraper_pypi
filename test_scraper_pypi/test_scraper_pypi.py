cdd16d6ce81f16d368cf269b4f52f1e90d703087
nova-sangeeth <novasangeeth@outlook.com>
2022-05-28 11:50:09 +0530

    Initial commit, Added Hello World for testing.

---
+++ test_scraper_pypi/test_scraper_pypi.py
@@ -0,0 +1,18 @@
+"""Main module."""
+
+
+class TestScraperPypi:
+    """Main class."""
+
+    def __init__(self):
+        """Constructor."""
+        pass
+
+    def main(self):
+        """Main function."""
+        return self.return_greet_message()
+
+    def return_greet_message(self):
+        """Return a greeting message."""
+        message = "Hello World!"
+        return message
